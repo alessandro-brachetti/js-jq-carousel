5d14a5eb8f122fe8e1b2ff962a2332b6a496abb8
Alessandro Brachetti <alebrack13@gmail.com>
2021-03-11 17:36:02 +0100

    Esercizio Completo

--- main.js
+++ main.js
@@ -9,22 +9,39 @@ function slider() {
 
   var carouselNext = $('.next')
   var carouselPrev = $('.prev')
-  var currentImg = $('.active')
-  var nextImg = currentImg.next();
-  var prevImg = currentImg.prev();
-
 
   carouselNext.click(function () {
+    var currentImg = $('.active');
+    var nextImg = currentImg.next();
+
     if (currentImg.hasClass('active')) {
       currentImg.removeClass('active');
       nextImg.addClass('active');
     }
+    var first = $('.first');
+    var last = $('.last');
+
+    if (currentImg.hasClass('last')) {
+    currentImg.removeClass('active');
+    first.addClass('active');
+    }
+
   });
 
   carouselPrev.click(function () {
+    var currentImg = $('.active');
+    var prevImg = currentImg.prev();
+
     if (currentImg.hasClass('active')) {
       currentImg.removeClass('active');
       prevImg.addClass('active');
+    }
+    var first = $('.first');
+    var last = $('.last');
+
+    if (currentImg.hasClass('first')) {
+    currentImg.removeClass('active');
+    last.addClass('active');
     }
   });
 
